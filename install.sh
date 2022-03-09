0e21238a8c457fdec06f1a4467b70dff4e61716b
zhongzf <zhongzf@126.com>
2022-03-09 23:24:39 +0800

    feat: add moonraker_setup.

--- install.sh
+++ install.sh
@@ -43,6 +43,24 @@ install_dependencies(){
         jq patch curl caddy nginx sudo openrc
 }
 
+klipper_setup(){
+  ### step 1: clone klipper
+  status_msg "Downloading Klipper ..."
+  ### clone into fresh klipper dir
+  cd "${HOME}" && git clone "$KLIPPER_REPO"
+  status_msg "Download complete!"
+
+  ### step 2: create python virtualenv
+  create_klipper_virtualenv
+
+  ### step 3: create shared gcode_files and logs folder
+  [ ! -d "${HOME}"/gcode_files ] && mkdir -p "${HOME}"/gcode_files
+  [ ! -d "${HOME}"/klipper_logs ] && mkdir -p "${HOME}"/klipper_logs
+
+  ### step 4: create klipper plugin
+  create_klipper_plugin
+}
+
 create_klipper_virtualenv(){
   status_msg "Installing python virtual environment..."
   # Create virtualenv if it doesn't already exist
@@ -52,55 +70,132 @@ create_klipper_virtualenv(){
 }
 
 create_klipper_plugin(){    
-    CFG_PATH="$klipper_cfg_loc"
-    KL_ENV=$KLIPPY_ENV
-    KL_DIR=$KLIPPER_DIR
-    KL_LOG="${HOME}/klipper_logs/klippy.log"
-    KL_UDS="/tmp/klippy_uds"
-    P_TMP="/tmp/printer"
-    P_CFG="$CFG_PATH/printer.cfg"
-    P_CFG_SRC="${SRCDIR}/klipper4a/resources/printer.cfg"
+  CFG_PATH="$klipper_cfg_loc"
+  KL_ENV=$KLIPPY_ENV
+  KL_DIR=$KLIPPER_DIR
+  KL_LOG="${HOME}/klipper_logs/klippy.log"
+  KL_UDS="/tmp/klippy_uds"
+  P_TMP="/tmp/printer"
+  P_CFG="$CFG_PATH/printer.cfg"
+  P_CFG_SRC="${SRCDIR}/klipper4a/resources/printer.cfg"
 
-    KL_EXTENSIONS_SRC="${SRCDIR}/klipper4a/resources/extensions/klipper"
-    KL_EXTENSIONS_TARGET="/root/extensions/klipper"
-    KL_START_TARGET="/root/extensions/klipper/start.sh"
+  KL_EXTENSIONS_SRC="${SRCDIR}/klipper4a/resources/extensions/klipper"
+  KL_EXTENSIONS_TARGET="/root/extensions/klipper"
 
-    mkdir -p $KL_EXTENSIONS_TARGET
-    cp "$KL_EXTENSIONS_SRC/manifest.json" "$KL_START_TARGET/manifest.json"
+  mkdir -p $KL_EXTENSIONS_TARGET
+  cp "$KL_EXTENSIONS_SRC/manifest.json" "$KL_EXTENSIONS_TARGET/manifest.json"
 
-    sudo cp "$KL_EXTENSIONS_SRC/start.sh" $KL_START_TARGET
-    sudo sed -i "s|%KL_ENV%|$KL_ENV|" $KL_START_TARGET
-    sudo sed -i "s|%KL_DIR%|$KL_DIR|" $KL_START_TARGET
-    sudo sed -i "s|%KL_LOG%|$KL_LOG|" $KL_START_TARGET
-    sudo sed -i "s|%P_CFG%|$P_CFG|" $KL_START_TARGET
-    sudo sed -i "s|%P_TMP%|$P_TMP|" $KL_START_TARGET
-    sudo sed -i "s|%KL_UDS%|$KL_UDS|" $KL_START_TARGET
+  KL_START_TARGET="/root/extensions/klipper/start.sh"
+  sudo cp "$KL_EXTENSIONS_SRC/start.sh" $KL_START_TARGET
+  sudo sed -i "s|%KL_ENV%|$KL_ENV|" $KL_START_TARGET
+  sudo sed -i "s|%KL_DIR%|$KL_DIR|" $KL_START_TARGET
+  sudo sed -i "s|%KL_LOG%|$KL_LOG|" $KL_START_TARGET
+  sudo sed -i "s|%P_CFG%|$P_CFG|" $KL_START_TARGET
+  sudo sed -i "s|%P_TMP%|$P_TMP|" $KL_START_TARGET
+  sudo sed -i "s|%KL_UDS%|$KL_UDS|" $KL_START_TARGET
+  chmod +x $KL_START_TARGET
+  chmod 777 $KL_START_TARGET
 
-    chmod +x $KL_START_TARGET
-    chmod 777 $KL_START_TARGET
-
-    cp "$KL_EXTENSIONS_SRC/kill.sh" "$KL_START_TARGET/kill.sh"
-    chmod +x "$KL_START_TARGET/kill.sh"
-    chmod 777 "$KL_START_TARGET/kill.sh"
+  cp "$KL_EXTENSIONS_SRC/kill.sh" "$KL_EXTENSIONS_TARGET/kill.sh"
+  chmod +x "$KL_EXTENSIONS_TARGET/kill.sh"
+  chmod 777 "$KL_EXTENSIONS_TARGET/kill.sh"
 }
 
-klipper_setup(){
-  ### step 1: clone klipper
-  status_msg "Downloading Klipper ..."
-  ### force remove existing klipper dir and clone into fresh klipper dir
-  [ -d "$KLIPPER_DIR" ] && rm -rf "$KLIPPER_DIR"
-  cd "${HOME}" && git clone "$KLIPPER_REPO"
-  status_msg "Download complete!"
+moonraker_setup(){
+  ### step 1: clone moonraker
+  status_msg "Downloading Moonraker ..."
+  ### clone into fresh moonraker dir
+  cd "${HOME}" && git clone $MOONRAKER_REPO
+  ok_msg "Download complete!"
 
-  ### step 2: install klipper dependencies and create python virtualenv
-  create_klipper_virtualenv
+  ### step 2: create python virtualenv
+  create_moonraker_virtualenv
 
-  ### step 3: create shared gcode_files and logs folder
-  [ ! -d "${HOME}"/gcode_files ] && mkdir -p "${HOME}"/gcode_files
-  [ ! -d "${HOME}"/klipper_logs ] && mkdir -p "${HOME}"/klipper_logs
+  ### step 3: create moonraker.conf folder and moonraker.confs
+  create_moonraker_conf
 
-  ### step 4: create klipper plugin
-  create_klipper_plugin
+  ### step 4: create moonraker plugin
+  create_moonraker_plugin
+}
+
+create_moonraker_virtualenv(){
+  status_msg "Installing python virtual environment..."
+  # Create virtualenv if it doesn't already exist
+  if [ ! -d "$MOONRAKER_ENV" ]; then
+    virtualenv -p /usr/bin/python3 "$MOONRAKER_ENV"
+  fi
+
+  ### Install/update dependencies
+  "$MOONRAKER_ENV"/bin/pip install -r "$MOONRAKER_DIR"/scripts/moonraker-requirements.txt
+}
+
+create_moonraker_conf(){
+  PORT=7125
+  CFG_PATH="$klipper_cfg_loc"
+  LOG_PATH="${HOME}/klipper_logs"
+  MR_CONF="$CFG_PATH/moonraker.conf"
+  MR_DB="${HOME}/.moonraker_database"
+  KLIPPY_UDS="/tmp/klippy_uds"
+  MR_CONF_SRC="${SRCDIR}/klipper4a/resources/moonraker.conf"
+  mr_ip_list=()
+  IP=$(hostname -I | cut -d" " -f1)
+  LAN="$(hostname -I | cut -d" " -f1 | cut -d"." -f1-2).0.0/16"
+
+  write_mr_conf(){
+    [ ! -d "$CFG_PATH" ] && mkdir -p "$CFG_PATH"
+    if [ ! -f "$MR_CONF" ]; then
+      status_msg "Creating moonraker.conf in $CFG_PATH ..."
+        cp "$MR_CONF_SRC" "$MR_CONF"
+        sed -i "s|%PORT%|$PORT|" "$MR_CONF"
+        sed -i "s|%CFG%|$CFG_PATH|" "$MR_CONF"
+        sed -i "s|%LOG%|$LOG_PATH|" "$MR_CONF"
+        sed -i "s|%MR_DB%|$MR_DB|" "$MR_CONF"
+        sed -i "s|%UDS%|$KLIPPY_UDS|" "$MR_CONF"
+        # if host ip is not in the default ip ranges, replace placeholder
+        # otherwise remove placeholder from config
+        if ! grep -q "$LAN" "$MR_CONF"; then
+          sed -i "s|%LAN%|$LAN|" "$MR_CONF"
+        else
+          sed -i "/%LAN%/d" "$MR_CONF"
+        fi
+        sed -i "s|%USER%|${USER}|g" "$MR_CONF"
+      ok_msg "moonraker.conf created!"
+    else
+      warn_msg "There is already a file called 'moonraker.conf'!"
+      warn_msg "Skipping..."
+    fi
+  }
+
+  ### write single instance config
+  write_mr_conf
+  mr_ip_list+=("$IP:$PORT")
+}
+
+create_moonraker_plugin(){    
+  CFG_PATH="$klipper_cfg_loc"
+  MR_ENV=$MOONRAKER_ENV
+  MR_DIR=$MOONRAKER_DIR
+  MR_LOG="${HOME}/klipper_logs/moonraker.log"
+  MR_CONF="$CFG_PATH/moonraker.conf"
+
+  MR_EXTENSIONS_SRC="${SRCDIR}/klipper4a/resources/extensions/moonraker"
+  MR_EXTENSIONS_TARGET="/root/extensions/moonraker"
+
+  mkdir -p $MR_EXTENSIONS_TARGET
+  cp "$MR_EXTENSIONS_SRC/manifest.json" "$MR_EXTENSIONS_TARGET/manifest.json"
+
+  MR_START_TARGET="/root/extensions/moonraker/start.sh"
+  sudo cp "$MR_EXTENSIONS_SRC/start.sh" $MR_START_TARGET
+  sudo sed -i "s|%MR_ENV%|$MR_ENV|" $MR_START_TARGET
+  sudo sed -i "s|%MR_DIR%|$MR_DIR|" $MR_START_TARGET
+  sudo sed -i "s|%MR_LOG%|$MR_LOG|" $MR_START_TARGET
+  sudo sed -i "s|%MR_CONF%|$MR_CONF|" $MR_START_TARGET
+  chmod +x $MR_START_TARGET
+  chmod 777 $MR_START_TARGET
+
+  cp "$MR_EXTENSIONS_SRC/kill.sh" "$MR_EXTENSIONS_TARGET/kill.sh"
+  chmod +x "$MR_EXTENSIONS_TARGET/kill.sh"
+  chmod 777 "$MR_EXTENSIONS_TARGET/kill.sh"
 }
 
 
@@ -119,25 +214,6 @@ symlink_webui_nginx_log(){
     ln -s "$UI_ERROR_LOG" "$LPATH"
     ok_msg "OK!"
   fi
-}
-
-mainsail_setup(){
-  ### get mainsail download url
-  MAINSAIL_DL_URL=$(curl -s $MAINSAIL_REPO_API | grep browser_download_url | cut -d'"' -f4 | head -1)
-
-  ### remove existing and create fresh mainsail folder, then download mainsail
-  [ -d "$MAINSAIL_DIR" ] && rm -rf "$MAINSAIL_DIR"
-  mkdir "$MAINSAIL_DIR" && cd $MAINSAIL_DIR
-  status_msg "Downloading Mainsail $MAINSAIL_VERSION ..."
-  wget "$MAINSAIL_DL_URL" && ok_msg "Download complete!"
-
-  ### extract archive
-  status_msg "Extracting archive ..."
-  unzip -q -o *.zip && ok_msg "Done!"
-
-  ### delete downloaded zip
-  status_msg "Remove downloaded archive ..."
-  rm -rf *.zip && ok_msg "Done!"
 }
 
 
@@ -160,5 +236,27 @@ fluidd_setup(){
   rm -rf *.zip && ok_msg "Done!"
 }
 
+mainsail_setup(){
+  ### get mainsail download url
+  MAINSAIL_DL_URL=$(curl -s $MAINSAIL_REPO_API | grep browser_download_url | cut -d'"' -f4 | head -1)
 
+  ### remove existing and create fresh mainsail folder, then download mainsail
+  [ -d "$MAINSAIL_DIR" ] && rm -rf "$MAINSAIL_DIR"
+  mkdir "$MAINSAIL_DIR" && cd $MAINSAIL_DIR
+  status_msg "Downloading Mainsail $MAINSAIL_VERSION ..."
+  wget "$MAINSAIL_DL_URL" && ok_msg "Download complete!"
+
+  ### extract archive
+  status_msg "Extracting archive ..."
+  unzip -q -o *.zip && ok_msg "Done!"
+
+  ### delete downloaded zip
+  status_msg "Remove downloaded archive ..."
+  rm -rf *.zip && ok_msg "Done!"
+}
+
+install_dependencies
 klipper_setup
+moonraker_setup
+fluidd_setup
+mainsail_setup
